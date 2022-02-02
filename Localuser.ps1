e2510c06a72d4f5dad5ec869660548c00f1a3eef
crabtrjh <crabtrjh@mail.uc.edu>
2022-02-02 09:13:21 -0500

    Initial commit

---
+++ Localuser.ps1
@@ -0,0 +1,9 @@
+﻿function getLocalUser{
+Get-LocalUser -name "Administrator"
+}
+
+write-host(Get-LocalUser)
+
+$LocalUser = Get-LocalUser -name "Administrator"
+
+write-host("User is: $LocalUser")
---
+++ powershell/email.ps1
@@ -0,0 +1,13 @@
+﻿function getIP{
+(Get-NetIPAddress).IPv4Address | select-string "192"
+}
+
+write-host(getIP)
+
+$IP = getIP
+
+
+
+
+
+Send-MailMessage -to "crabtrjh@mail.uc.edu" -from "joshuacrabtree32@gmail.com" -subject "IT3038c Windows SysInfo" -Body "My machines ip address is: $IP" -SmtpServer smtp.gmail.com -port 587 -UseSsl -Credential (Get-Credential)
