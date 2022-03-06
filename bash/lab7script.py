30cf26a370bb35e8cfa78af145f4777d8c4653ce
Student <crabtrjh@mail.uc.edu>
2022-03-06 15:52:46 -0500

    lab7

--- bash/lab7script.py
+++ bash/lab7script.py
@@ -1,2 +1,8 @@
-biblehub find Matthew 2:4
 #this script scraps biblehub.com and displays the chosen verse.
+
+biblehub find Matthew 2:4
+
+#Example uses
+#1. Seacrching for spesific bible verses.
+#2. Cross referencing different sections.
+#3. Checking different versions of the bible and comparing them to get a better understanding.
